cf6ff4cd2018a2ad289231d8c53ac3812e580f37
Anastasiya Bukata <anastasijagrig16.08.1993@gmail.com>
2022-02-24 22:10:19 +0300

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = 0;
+  let close = 0;
+
+  bracketsConfig.forEach((pattern) => {
+    for (i = 0; i < str.length; i++) {
+      if (str[i] === pattern[0] && str[i] === pattern[1]) {
+        open++;
+        close++;
+      }
+      else if (str[i] === pattern[0]) {
+        open++;
+      }
+      else if (str[i] === pattern[1]) {
+        close++;
+      }
+    }
+  })
+
+  if (open === close) {
+    return true;
+  } else {
+    return false;
+  }
 }
